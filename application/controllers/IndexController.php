025d125a620c9939125b1ac24d2900b3d27d2d2e
root <root@VIR104.(none)>
2012-03-01 12:26:26 +0530

    Deleted: Unwanted Files & Other changes

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,7 +10,9 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+        //Create Session Namespace object & check if Session is Created or not
+        $oNamespace = new Zend_Session_Namespace('sess_admin');        
+       	$this->view->ssLoginUserName = $ssLoginUserName = $oNamespace->__get('admin_name');
     }
 
 
