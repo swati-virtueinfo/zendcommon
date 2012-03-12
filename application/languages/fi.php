c89bbec0ca1533d8d91b9bddeaa3ce5f73b38923
Bhaskar Joshi <bhaskar.virtueinfo@gmail.com>
2012-03-12 18:07:53 +0530

    Done: Add sitepage module & Changes all module design & city testcases

--- application/languages/fi.php
+++ application/languages/fi.php
@@ -1,13 +1,13 @@
 <?php
 return array(
-	'test' => 'test_fi',
-	'admin_Admin_variabl' => 'admin_Admin_variable_fi',
-	'btn_Button_variable' => 'btn_Button_variable_fi',
-	'client_Client_variable' => 'client_Client_variable_fi',
-	'lbl_Label_variable' => 'lbl_Label_variable_fi',
-	'lnk_Link_varable' => 'lnk_Link_varable_fi',
-	'menu_Menu_variable' => 'menu_Menu_variable_fi',
-	'msg_Message_variable' => 'msg_Message_variable_fi',
-	'note_Note_variable' => 'note_Note_variable_fi',
-	'ttl_Title_variable' => 'ttl_Title_variable_fi',
+	'' => '',
+	'Action' => 'Action_fi',
+	'Active' => 'Active_fi',
+	'Country Name' => 'Country Name_fi',
+	'Edit' => 'Edit_fi',
+	'Enable Or Disable' => 'Enable Or Disable_fi ',
+	'lbl_delete' => 'saf',
+	'lbl_serach_by_value' => 'asda',
+	'lnk_add_new_variable' => 'delete_fid',
+	'lnk_value' => 'dsaf',
 );
